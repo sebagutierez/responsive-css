f243f0138b449bca544858314eab3309b1d9fdb3
Sebastian Gutierrez <sebagutierez@gmail.com>
2022-12-22 11:22:12 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".drop-down-menu");
+const hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  menu.classList.toggle("show");
+});
